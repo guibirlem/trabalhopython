282458c6d5e436d8e337e4fd8493f2014e5ec0dd
“guibirlem” <guisperto@hotmail.com>
2024-07-03 00:15:30 -0300

    iniciei o menu do banco python

--- trabalho.py
+++ trabalho.py
@@ -7,6 +7,7 @@ contas = {}
 
 #mudança feita 
 #mudança feita novamente 341212313231212
+# mudança feita 00 14 horario
 def cadastrar_usuario(nome, cpf):
     if cpf in usuarios:
         print("Usuário já cadastrado.")
